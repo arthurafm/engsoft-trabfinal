89b6243e6fdd5b2539d1e2930de82017a4331aa8
Eduardo Faé <eduardofaeef@gmail.com>
2023-08-14 15:12:41 -0300

    Adição do componente do botão

---
+++ src/components/button.tsx
@@ -0,0 +1,30 @@
+'use client'
+
+import Button from '@mui/material/Button'
+
+export default function Butt({text, width, height, fontSize}) {
+    const style = {
+        color: 'white',
+        background: 'primary.light',
+        borderRadius: '1rem',
+        textTransform: 'none',
+        border: 1,
+        borderColor: 'transparent',
+        fontWeight: 600,
+        p: 0,
+
+        width: {width},
+        height: {height},
+        fontSize: {fontSize},
+        
+        '&:hover': {
+            backgroundColor: 'white',
+            color: '#E35725',
+            borderColor: '#FF7222',
+        },
+    }
+
+	return (
+        <Button variant='contained' sx={style}>{text}</Button>
+	)
+}
