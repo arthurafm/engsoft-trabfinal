960de19602ed3ffed8588a801b593aeebf22f1cb
Arthur <arthuralvesfmelo@gmail.com>
2023-08-23 13:56:19 -0300

    Corrigido tipos e adicionados dividers

--- src/components/courses/tutorSchedule.tsx
+++ src/components/courses/tutorSchedule.tsx
@@ -8,7 +8,11 @@ import Link from 'next/link'
 
 import { useState } from 'react'
 
-const TutorSchedule = (isProfessor: boolean) => {
+interface Props {
+    isProfessor: boolean
+}
+
+const TutorSchedule = ({isProfessor}: Props) => {
 
     const [tutors, setTutors] = useState (
         [
@@ -57,132 +61,136 @@ const TutorSchedule = (isProfessor: boolean) => {
 	};
 
     const handleSubmit = () => {
-        setTutors(tutors.append());
+        setTutors([...tutors, ]);
     }
 
 
     return (
-        <Box
-            sx={{
-                display: 'flex',
-                flexDirection: 'column',
-                alignItems: 'center',
-                pr: '30%',
-                pl: '30%',
-            }}>
-            <Divider />
-            <Typography
-                variant='h3'
+        <>
+            <Divider sx={{ mt: 3, mb: 2,}} />
+            <Box
                 sx={{
-                    fontWeight: 700,
-                    color: '#C73700',
-                    pb: 3,
+                    display: 'flex',
+                    flexDirection: 'column',
+                    alignItems: 'center',
+                    pr: '30%',
+                    pl: '30%',
                 }}>
-                Monitores
-            </Typography>
-            <Divider />
-            <TableContainer
-                component={Paper}
-                sx={{
-                    mb: 3,
-                }}>
-                <Table>
-                    <TableHead>
-                        <TableRow>
-                            <TableCell align="center">Monitor</TableCell>
-                            <TableCell align="center">Horário</TableCell>
-                            <TableCell align="center">Dias</TableCell>
-                            <TableCell align="center">Link para reunião</TableCell>
-                        </TableRow>
-                    </TableHead>
-                    <TableBody>
-                        {
-                            tutors.map((tutor, i) => {
-                                return (
-                                    <TableRow
-                                        key={i}
-                                        sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
-                                        <TableCell component="th" scope="row" align="center">{tutor.name}</TableCell>
-                                        <TableCell align="center">{tutor.schedule}</TableCell>
-                                        <TableCell align="center">{tutor.days}</TableCell>
-                                        <TableCell align="center">
-                                            <IconButton href={tutor.link}>
-                                                <VideoCameraFront />
-                                            </IconButton>
-                                        </TableCell>
-                                    </TableRow>
-                                )
-                            })
-                        }
-                    </TableBody>
-                </Table>
-            </TableContainer>
-            {
-                isProfessor &&
-                    <div>
-                        <Button
-                        variant="contained"
-                        onClick={handleClickOpen}
-                        sx = {{
-                            color: 'white',
-                            fontSize: '1.2rem',
-                            background: '#E35725',
-                            borderRadius: '1rem',
-                            textTransform: 'none',
-                            border: 1,
-                            borderColor: 'transparent',
-                            fontWeight: 600,
-                            width: '13rem',
-                            height: '3rem',
-                            '&:hover': {
-                                backgroundColor: 'white',
-                                color: '#E35725',
-                                borderColor: '#FF7222',
-                            },
-                            mb: 6,
-                        }}
-                        >
-                            Adicionar Monitor
-                        </Button>
-                        <Dialog onClose={handleClose} open={open} fullWidth={true} maxWidth={'xs'}>
-                            <DialogTitle fontSize={25}>Adicione um novo monitor</DialogTitle>
-                            <form onSubmit={handleSubmit}>
-                                <Stack sx={{mr: 4, ml: 4}} spacing={2}>
-                                    <Autocomplete
-                                        disablePortal
-                                        id="students"
-                                        options={studentsList}
-                                        renderInput={(params) => <TextField {...params} label="Alunos" />}
-                                    />
-                                    <TextField
-                                        id='schedule'
-                                        variant='outlined'
-                                        label='Horário do monitor'
-                                        type='text'
-                                        margin='dense' />
-                                    <TextField
-                                        id='days'
-                                        variant='outlined'
-                                        label='Dias disponíveis do monitor'
-                                        type='text'
-                                        margin='dense' />
-                                    <TextField
-                                        id='link'
-                                        variant='outlined'
-                                        label='Link para reunião com monitor'
-                                        type='text'
-                                        margin='dense' />
+                <Divider />
+                <Typography
+                    variant='h3'
+                    sx={{
+                        fontWeight: 700,
+                        color: '#C73700',
+                        pb: 3,
+                    }}>
+                    Monitores
+                </Typography>
+                <Divider />
+                <TableContainer
+                    component={Paper}
+                    sx={{
+                        mb: 3,
+                    }}>
+                    <Table>
+                        <TableHead>
+                            <TableRow>
+                                <TableCell align="center">Monitor</TableCell>
+                                <TableCell align="center">Horário</TableCell>
+                                <TableCell align="center">Dias</TableCell>
+                                <TableCell align="center">Link para reunião</TableCell>
+                            </TableRow>
+                        </TableHead>
+                        <TableBody>
+                            {
+                                tutors.map((tutor, i) => {
+                                    return (
+                                        <TableRow
+                                            key={i}
+                                            sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
+                                            <TableCell component="th" scope="row" align="center">{tutor.name}</TableCell>
+                                            <TableCell align="center">{tutor.schedule}</TableCell>
+                                            <TableCell align="center">{tutor.days}</TableCell>
+                                            <TableCell align="center">
+                                                <IconButton href={tutor.link}>
+                                                    <VideoCameraFront />
+                                                </IconButton>
+                                            </TableCell>
+                                        </TableRow>
+                                    )
+                                })
+                            }
+                        </TableBody>
+                    </Table>
+                </TableContainer>
+                {
+                    isProfessor &&
+                        <div>
+                            <Button
+                            variant="contained"
+                            onClick={handleClickOpen}
+                            sx = {{
+                                color: 'white',
+                                fontSize: '1.2rem',
+                                background: '#E35725',
+                                borderRadius: '1rem',
+                                textTransform: 'none',
+                                border: 1,
+                                borderColor: 'transparent',
+                                fontWeight: 600,
+                                width: '13rem',
+                                height: '3rem',
+                                '&:hover': {
+                                    backgroundColor: 'white',
+                                    color: '#E35725',
+                                    borderColor: '#FF7222',
+                                },
+                                mb: 6,
+                            }}
+                            >
+                                Adicionar Monitor
+                            </Button>
+                            <Dialog onClose={handleClose} open={open} fullWidth={true} maxWidth={'xs'}>
+                                <DialogTitle fontSize={25}>Adicione um novo monitor</DialogTitle>
+                                <form onSubmit={handleSubmit}>
+                                    <Stack sx={{mr: 4, ml: 4}} spacing={2}>
+                                        <Autocomplete
+                                            disablePortal
+                                            id="students"
+                                            options={studentsList}
+                                            renderInput={(params) => <TextField {...params} label="Alunos" />}
+                                        />
+                                        <TextField
+                                            id='schedule'
+                                            variant='outlined'
+                                            label='Horário do monitor'
+                                            type='text'
+                                            margin='dense' />
+                                        <TextField
+                                            id='days'
+                                            variant='outlined'
+                                            label='Dias disponíveis do monitor'
+                                            type='text'
+                                            margin='dense' />
+                                        <TextField
+                                            id='link'
+                                            variant='outlined'
+                                            label='Link para reunião com monitor'
+                                            type='text'
+                                            margin='dense' />
 
-                                    <Button variant='contained' type='submit'>
-                                        <Typography variant='h6' color='white'>Adicionar</Typography> 
-                                    </Button>
-                                    <Box sx ={{ pb: 2,}} />
-                                </Stack>
-                            </form>
-                        </Dialog>
-                    </div>
-            }
-        </Box>
+                                        <Button variant='contained' type='submit'>
+                                            <Typography variant='h6' color='white'>Adicionar</Typography> 
+                                        </Button>
+                                        <Box sx ={{ pb: 2,}} />
+                                    </Stack>
+                                </form>
+                            </Dialog>
+                        </div>
+                }
+            </Box>
+            <Divider sx={{ mt: 3, mb: 2,}} />
+        </>
     )
 }
 
