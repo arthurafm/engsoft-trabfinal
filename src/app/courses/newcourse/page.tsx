85dcd6eb36b65771c38dd684727d1b7d9589bf96
Eduardo Faé <eduardofaeef@gmail.com>
2023-08-23 00:33:04 -0300

    Página de Criação de Curso Feita

--- src/app/courses/newcourse/page.tsx
+++ src/app/courses/newcourse/page.tsx
@@ -1,8 +1,8 @@
 'use client'
 
 import { useUser } from '@/context/UserContext';
-import { Stack, TextField, Button, IconButton, Typography } from '@mui/material'
-import DeleteIcon from '@mui/icons-material/Delete';
+import { Stack, TextField, Button, IconButton, Typography, Box, Accordion, AccordionSummary, AccordionDetails } from '@mui/material'
+import { Delete, ExpandMore } from '@mui/icons-material';
 import { useEffect, useState } from 'react';
 import { useRouter } from 'next/navigation'
 
@@ -41,13 +41,40 @@ mutation CreateModulo(
 
 export default function Page(){
 	const { cognitoUser, userData } = useUser()
-	if(!cognitoUser || userData?.__typename != "Professor"){
-		return <div>Ops, você não é professor</div>
-	}
+	// if(!cognitoUser || userData?.__typename != "Professor"){
+	// 	return <div>Ops, você não é professor</div>
+	// }
 	const { register, handleSubmit, formState: { errors } } = useForm<IFormCriarCurso>() as any;
 	const router = useRouter()
 	const [modulos, setModulos] = useState<number[]>([])
 	const [moduloId, setModuloId] = useState(1)
+
+	const styles = {
+		Buttons : {
+			color: 'white',
+			background: 'primary.light',
+			borderRadius: '1rem',
+			textTransform: 'none',
+			border: 1,
+			borderColor: 'transparent',
+			fontWeight: 600,
+			py: 1,
+			px: 3,
+
+			width: 'fit-content',
+			height: 'fit-content',
+			fontSize: 20,
+
+			'&:hover': {
+				backgroundColor: 'white',
+				color: '#E35725',
+				borderColor: '#FF7222',
+			},
+		},
+		Inputs : {
+			width: '100%'
+		} 
+    }
 
 	useEffect(()=>{
 		console.log(modulos)
@@ -66,160 +93,186 @@ export default function Page(){
 	const onSubmit = async (data: IFormCriarCurso) =>{
 		console.log(data)
 
-		for(const id of modulos){
-			if(!data[`titulo-${id}`] || !data[`descricao-${id}`] || !data[`videolink-${id}`]){
-				console.log("Input nula da entrada " + id)
-				return
-			}
-		}
-		let cursoId = undefined
-		/*
-		const qEcho = await API.graphql({
-			query: echo,
-			variables: {msg: "hello"},
-			authMode: GRAPHQL_AUTH_MODE.AMAZON_COGNITO_USER_POOLS
-		})
-		console.log(qEcho)
-		*/
-		try{
-			const vars: CriarCursoMutationVariables = {
-				nome: data['titulo'],
-				preco: Number(data['preco']),
-				descricao: data['descricao']
-			}
-			console.log(vars)
-			const criarCurso = (await API.graphql({
-				query: customCriarCurso,
-				variables: vars,
-				authMode: GRAPHQL_AUTH_MODE.AMAZON_COGNITO_USER_POOLS
-			})) as { data: CriarCursoMutation } 
-			cursoId = criarCurso.data.criarCurso.id
-			if(!cursoId){
-				throw new Error("Criação de curso resultou em nenhum id")
-			}
-		}catch(error: any){
-			console.error(error)
-			return
-		}
-		let modulePromisses = []
-		for(const id of modulos){
-			const vars: CreateModuloInput = {
-				titulo: data[`titulo-${id}`] as string,
-				descricao: data[`descricao-${id}`] as string,
-				videoLink: data[`videolink-${id}`] ? data[`videolink-${id}`] as string : "",
-			}
-			modulePromisses.push(API.graphql({
-				query: customCreateModulo,
-				variables: {input: vars},
-				authMode: GRAPHQL_AUTH_MODE.AMAZON_COGNITO_USER_POOLS
-			}))
-		}
-		await Promise.all(modulePromisses)
+		// for(const id of modulos){
+		// 	if(!data[`titulo-${id}`] || !data[`descricao-${id}`] || !data[`videolink-${id}`]){
+		// 		console.log("Input nula da entrada " + id)
+		// 		return
+		// 	}
+		// }
+		// let cursoId = undefined
+		// /*
+		// const qEcho = await API.graphql({
+		// 	query: echo,
+		// 	variables: {msg: "hello"},
+		// 	authMode: GRAPHQL_AUTH_MODE.AMAZON_COGNITO_USER_POOLS
+		// })
+		// console.log(qEcho)
+		// */
+		// try{
+		// 	const vars: CriarCursoMutationVariables = {
+		// 		nome: data['titulo'],
+		// 		preco: Number(data['preco']),
+		// 		descricao: data['descricao']
+		// 	}
+		// 	console.log(vars)
+		// 	const criarCurso = (await API.graphql({
+		// 		query: customCriarCurso,
+		// 		variables: vars,
+		// 		authMode: GRAPHQL_AUTH_MODE.AMAZON_COGNITO_USER_POOLS
+		// 	})) as { data: CriarCursoMutation } 
+		// 	cursoId = criarCurso.data.criarCurso.id
+		// 	if(!cursoId){
+		// 		throw new Error("Criação de curso resultou em nenhum id")
+		// 	}
+		// }catch(error: any){
+		// 	console.error(error)
+		// 	return
+		// }
+		// let modulePromisses = []
+		// for(const id of modulos){
+		// 	const vars: CreateModuloInput = {
+		// 		titulo: data[`titulo-${id}`] as string,
+		// 		descricao: data[`descricao-${id}`] as string,
+		// 		videoLink: data[`videolink-${id}`] ? data[`videolink-${id}`] as string : "",
+		// 	}
+		// 	modulePromisses.push(API.graphql({
+		// 		query: customCreateModulo,
+		// 		variables: {input: vars},
+		// 		authMode: GRAPHQL_AUTH_MODE.AMAZON_COGNITO_USER_POOLS
+		// 	}))
+		// }
+		// await Promise.all(modulePromisses)
 		
-		router.push(`/courses/${cursoId}`)
+		// router.push(`/courses/${cursoId}`)
 	}
 
-	return <Stack spacing={6} alignItems='center' sx={{mt: 10, mb: 10}}>
-		<Typography variant='h3'>Criação de Curso</Typography>
+	return <Stack spacing={6} alignItems='center' sx={{ minHeight:'82vh' }}>
+		<Typography variant='h3' mt={10} fontFamily='Roboto' fontSize={60} fontWeight={700} color='#C73700'>Criação de Curso</Typography>
+		<Box pb={10} width='40%'>
 		<form onSubmit={handleSubmit(onSubmit)}>
 			<Stack spacing={4} alignItems='center'>
-				<TextField 
-					id='titulo'
-					variant='outlined'
-					label='Título do Curso'
-					type='text'
-					margin='dense'
-					error={errors['titulo'] ? true : false}
-					helperText={errors['titulo'] ? errors['titulo'].message : null}
-					{...register("titulo", {
-						required: { value: true, message: "Insira o Título" },
-						minLength: { value: 8, message: "Títulos devem ter pelo menos 8 caracteres"},
-						maxLength: { value: 30, message: "Títulos devem ter no máximo 30 caracteres"},
-					})}
-				/>
-				<TextField 
-					id='descricao'
-					variant='outlined'
-					label='Descrição do Curso'
-					type='text'
-					margin='dense'
-					error={errors['descricao'] ? true : false}
-					helperText={errors['descricao'] ? errors['descricao'].message : null}
-					{...register("descricao", {
-						required: { value: true, message: "Insira uma descrição" },
-						minLength: { value: 10, message:  "Descrições devem ter pelo menos 20 caracteres"},
-						maxLength: { value: 200, message: "Descrições devem ter no máximo 200 caracteres"},
-					})}
-				/>
-				<TextField 
-					id='preco'
-					variant='outlined'
-					label='Preco do Curso'
-					type='number'
-					margin='dense'
-					error={errors.preco ? true : false}
-					helperText={errors.preco ? errors.preco.message : null}
-					{...register("preco", {
-						required: { value: true, message: "Insira um Valor" },
-					})}
-				/>
-				{modulos.map((i, index) => {
-					return <Stack key={index} sx={{mt: 10, background: 'light grey'}}>
-						<Typography variant='h6' key={`title-${index}`}>
-							Modulo {index + 1}
-							<IconButton key={`del-${index}`} onClick={() => removeModulo(i)}>
-								<DeleteIcon key={`delico-${index}`}/>
-							</IconButton>
-						</Typography>
-						<TextField key={`titulo-${index}`}
-							id={`titulo-${i}`}
+				<Accordion sx={{width: '100%'}}>
+					<AccordionSummary
+					expandIcon={<ExpandMore />}
+					aria-controls="panel1a-content"
+					id="panel1a-header"
+					>
+					<Typography variant='h6' fontFamily='Roboto' fontWeight={700} color='#C73700'>
+						Informações Gerais
+					</Typography>
+					</AccordionSummary>
+					<AccordionDetails>
+						<TextField 
+							id='titulo'
 							variant='outlined'
-							label='Título do Módulo'
+							label='Título do Curso'
 							type='text'
 							margin='dense'
-							sx={{mr: 2, ml: 2}}
-							error={errors[`titulo-${i}`] ? true : false}
-							helperText={errors[`titulo-${i}`] ? errors[`titulo-${i}`]?.message : null}
-							{...register(`titulo-${i}`, {
+							error={errors['titulo'] ? true : false}
+							helperText={errors['titulo'] ? errors['titulo'].message : null}
+							sx={styles.Inputs}
+							{...register("titulo", {
 								required: { value: true, message: "Insira o Título" },
-								maxLength: { value: 30, message: "Títulos de modulo devem ter no máximo 30 caracteres"},
+								minLength: { value: 8, message: "Títulos devem ter pelo menos 8 caracteres"},
+								maxLength: { value: 30, message: "Títulos devem ter no máximo 30 caracteres"},
 							})}
 						/>
-						<TextField key={`descricao-${index}`}
-							id={`descricao-${i}`}
+						<TextField 
+							id='descricao'
 							variant='outlined'
 							label='Descrição do Curso'
 							type='text'
 							margin='dense'
-							sx={{mr: 2, ml: 2}}
-							error={errors[`descricao-${i}`] ? true : false}
-							helperText={errors[`descricao-${i}`] ? errors[`descricao-${i}`]?.message : null}
-							{...register(`descricao-${i}`, {
+							error={errors['descricao'] ? true : false}
+							helperText={errors['descricao'] ? errors['descricao'].message : null}
+							sx={styles.Inputs}
+							{...register("descricao", {
 								required: { value: true, message: "Insira uma descrição" },
-								minLength: { value: 20, message:  "Descrições devem ter pelo menos 20 caracteres"},
+								minLength: { value: 10, message:  "Descrições devem ter pelo menos 20 caracteres"},
 								maxLength: { value: 200, message: "Descrições devem ter no máximo 200 caracteres"},
 							})}
 						/>
-						<TextField key={`link-${index}`}
-							id={`videolink-${i}`}
+						<TextField 
+							id='preco'
 							variant='outlined'
-							label='link do video'
-							type='text'
+							label='Preco do Curso'
+							type='number'
 							margin='dense'
-							sx={{mr: 2, ml: 2}}
-							error={errors[`videolink-${i}`] ? true : false}
-							helperText={errors[`videolink-${i}`] ? errors[`videolink-${i}`]?.message : null}
-							{...register(`videolink-${i}`,{
+							error={errors.preco ? true : false}
+							helperText={errors.preco ? errors.preco.message : null}
+							sx={styles.Inputs}
+							{...register("preco", {
 								required: { value: true, message: "Insira um Valor" },
 							})}
 						/>
-					</Stack>
+					</AccordionDetails>
+				</Accordion>
+				{modulos.map((i, index) => {
+					return <Accordion key={index} sx={{width: '100%'}}>
+						<AccordionSummary
+						expandIcon={<ExpandMore />}
+						aria-controls="panel1a-content"
+						id="panel1a-header"
+						>
+						<Typography variant='h6' key={`title-${index}`} fontFamily='Roboto' fontWeight={700} color='#C73700'>
+							Modulo {index + 1}
+							<IconButton key={`del-${index}`} onClick={() => removeModulo(i)}>
+								<Delete key={`delico-${index}`}/>
+							</IconButton>
+						</Typography>
+						</AccordionSummary>
+							<AccordionDetails>
+							<TextField key={`titulo-${index}`}
+								id={`titulo-${i}`}
+								variant='outlined'
+								label='Título do Módulo'
+								type='text'
+								margin='dense'
+								sx={styles.Inputs}
+								error={errors[`titulo-${i}`] ? true : false}
+								helperText={errors[`titulo-${i}`] ? errors[`titulo-${i}`]?.message : null}
+								{...register(`titulo-${i}`, {
+									required: { value: true, message: "Insira o Título" },
+									maxLength: { value: 30, message: "Títulos de modulo devem ter no máximo 30 caracteres"},
+								})}
+							/>
+							<TextField key={`descricao-${index}`}
+								id={`descricao-${i}`}
+								variant='outlined'
+								label='Descrição do Módulo'
+								type='text'
+								margin='dense'
+								sx={styles.Inputs}
+								error={errors[`descricao-${i}`] ? true : false}
+								helperText={errors[`descricao-${i}`] ? errors[`descricao-${i}`]?.message : null}
+								{...register(`descricao-${i}`, {
+									required: { value: true, message: "Insira uma descrição" },
+									minLength: { value: 20, message:  "Descrições devem ter pelo menos 20 caracteres"},
+									maxLength: { value: 200, message: "Descrições devem ter no máximo 200 caracteres"},
+								})}
+							/>
+							<TextField key={`link-${index}`}
+								id={`videolink-${i}`}
+								variant='outlined'
+								label='link do video'
+								type='text'
+								margin='dense'
+								sx={styles.Inputs}
+								error={errors[`videolink-${i}`] ? true : false}
+								helperText={errors[`videolink-${i}`] ? errors[`videolink-${i}`]?.message : null}
+								{...register(`videolink-${i}`,{
+									required: { value: true, message: "Insira um Valor" },
+								})}
+							/>
+						</AccordionDetails>
+					</Accordion>
 				})}
-				<Stack direction='row' spacing={2}>
-					<Button type='submit' variant='contained'>Criar Curso</Button>
-					<Button variant='contained' onClick={addModulo}>Adicionar Módulo</Button>
+				<Stack direction='row' spacing={20} mb={10}>
+					<Button variant='contained' onClick={addModulo} sx={styles.Buttons}>Adicionar Módulo</Button>
+					<Button type='submit' variant='contained' sx={styles.Buttons}>Criar Curso</Button>
 				</Stack>
 			</Stack>
 		</form>
+		</Box>
 	</Stack>
 }
